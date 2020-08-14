efc4554ef0f7839bb72fbca47003ada35c623434
LISA_COOK <lisabpink@gmail.com>
2020-08-14 13:41:35 -0400

    names stretch

--- names/names.py
+++ names/names.py
@@ -1,7 +1,7 @@
 
 #!start runtime: 7.431328058242798 seconds
-#! end runtime: 0.15869998931884766 seconds
-
+#! end MVP runtime: 0.15869998931884766 seconds
+#! end STRETCH runtime: 0.005056142807006836 seconds
 
 from bst import BSTNode
 import time
@@ -24,6 +24,7 @@ duplicates = []  # Return the list of duplicates in this data structure
 #         if name_1 == name_2:
 #             duplicates.append(name_1)
 
+print("\n\n------------------------------MVP----------------------------\n")
 bst = BSTNode("")
 for name_1 in names_1:
     bst.insert(name_1)
@@ -33,10 +34,31 @@ for name_2 in names_2:
         duplicates.append(name_2)
 
 end_time = time.time()
-print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
-print (f"runtime: {end_time - start_time} seconds")
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n")
+print (f"runtime: {end_time - start_time} seconds\n")
 
 # ---------- Stretch Goal -----------
 # Python has built-in tools that allow for a very efficient approach to this problem
 # What's the best time you can accomplish?  Thare are no restrictions on techniques or data
 # structures, but you may not import any additional libraries that you did not write yourself.
+
+
+start_time = time.time()
+
+f = open('names_1.txt', 'r')
+names_1 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+f = open('names_2.txt', 'r')
+names_2 = f.read().split("\n")  # List containing 10000 names
+f.close()
+
+duplicates = []  # Return the list of duplicates in this data structure
+
+# Replace the nested for loops below with your improvements
+print("-----------------------------STRETCH-----------------------------\n")
+duplicates = list(set(names_1) & set(names_2))
+
+end_time = time.time()
+print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n")
+print (f"runtime: {end_time - start_time} seconds\n") 
